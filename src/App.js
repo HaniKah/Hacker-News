0bf2fc087172e9b2c80ed0f7f195d9cb6a142329
Stefan Lemke <stban@posteo.de>
2023-04-21 11:17:23 +0200

    Added pagination and footer

--- src/App.js
+++ src/App.js
@@ -1,47 +1,54 @@
 import { useState, useEffect } from "react";
 import Row from "./components/Row";
 import "./App.css";
+import Pagination from "@mui/material/Pagination";
+import Footer from "./components/Footer";
 
 function App() {
   const [posts, setPosts] = useState([]);
 
   const [searchTerm, setSearchTerm] = useState();
-
-  const [page, setPage] = useState(0);
-
-  // const [searchTerm, setSearchTerm] = useState("");
-
-  // const posts = fetch("http://hn.algolia.com/api/v1/search?query=")
-  //   .then((res) => res.json())
-  //   .then((data) => {
-  //     setPosts(data)
-  //   });
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
     fetchData();
   }, []);
 
+  const handlePageChange = (e, p) => {
+    console.log("clicked on page " + e, p);
+    setPage(p);
+    fetchData(searchTerm, p);
+  };
+
+  console.log("new value of page " + page);
+
   const handleChange = (event) => {
     let value = event.target.value;
 
-    // setSearchTerm(value);
+    setSearchTerm(value);
     fetchData(value);
   };
 
-  const fetchData = async (searchT) => {
+  const fetchData = async (searchT, page) => {
     let url = "http://hn.algolia.com/api/v1/search?query=";
 
     if (searchT) {
       url = url + searchT;
+      console.log("inside fetch if(searchT) " + url);
     }
-
-    console.log(url);
+    if (page) {
+      url = url + "&page=" + page;
+      console.log("inside fetch if(page) " + url);
+    } else {
+      setPage(1);
+    }
 
     const postsResponse = await fetch(url);
     const postsData = await postsResponse.json();
 
     setPosts(postsData.hits);
-    // console.log(postsData);
+
+    console.log("Data fetched " + url);
   };
 
   return (
@@ -54,6 +61,15 @@ function App() {
           </>
         ))}
       </ol>
+
+      <Pagination
+        boundaryCount={1}
+        siblingCount={3}
+        count={100}
+        page={page}
+        onChange={handlePageChange}
+      />
+      <Footer />
     </div>
   );
 }
